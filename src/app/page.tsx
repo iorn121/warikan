8f696b1823f10f8e82f526b668524d5cf9899b6e
iorn121 <wmt.tkn.121@gmail.com>
2023-08-02 23:31:55 +0900

    メイン機能実装完了

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,43 @@
 "use client";
 import styles from "./page.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type User = {
   id: number;
   name: string;
   buy: number;
   pay: number;
+  topay: boolean;
 };
 
+const STORAGE_KEY_DARK_MODE = "myapp.example.com/darkMode";
+
 export default function Home() {
+  // localStorageに記録する
   const [allUsers, setAllUsers] = useState<User[]>([]);
+  const [userToPay, setUserToPay] = useState(0);
+  // localStorageに記録しない
   const [userName, setUserName] = useState("");
+  const [userPay, setUserPay] = useState(0);
+  const [payPerson, setPayPerson] = useState(0);
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    if (!isLoaded) {
+      const stragedAllUsers = localStorage.getItem("allUsers");
+      const stragedUserToPay = localStorage.getItem("userToPay");
+      if (stragedAllUsers !== null) {
+        setAllUsers(JSON.parse(stragedAllUsers));
+      }
+      if (stragedUserToPay !== null) {
+        setUserToPay(JSON.parse(stragedUserToPay));
+      }
+      setIsLoaded(true);
+    } else {
+      localStorage.setItem("allUsers", JSON.stringify(allUsers));
+      localStorage.setItem("userToPay", JSON.stringify(userToPay));
+    }
+  }, [allUsers, userToPay]);
+
   return (
     <main className={styles.main}>
       <input
@@ -19,26 +45,117 @@ export default function Home() {
         onChange={(event) => setUserName(event.target.value)}
       />
       <button
-        onClick={() =>
+        onClick={() => {
           setAllUsers([
             ...allUsers,
-            { id: allUsers.length, name: userName, buy: 0, pay: 0 },
-          ])
-        }
+            {
+              id: allUsers.length,
+              name: userName,
+              buy: 0,
+              pay: 0,
+              topay: true,
+            },
+          ]);
+          setUserToPay(userToPay + 1);
+        }}
       >
         Add User
       </button>
-      <table>
-        {allUsers.map((user) => {
-          return (
-            <tr className="styles.user">
-              <td>{user.name}</td>
-              <td>{String(user.pay)}</td>
-              <td>{String(user.buy)}</td>
-            </tr>
-          );
+      <label>支払い対象</label>
+      {allUsers.map((user, i) => {
+        return (
+          <div>
+            <p>{user.name}</p>
+            <input
+              type="checkbox"
+              defaultChecked={user.topay}
+              onChange={(event) => {
+                setAllUsers(
+                  allUsers.map((user, j) => {
+                    if (i == j) {
+                      return { ...user, topay: event.target.checked };
+                    }
+                    if (user.topay)
+                      setUserToPay(userToPay + (event.target.checked ? 1 : -1));
+                    return user;
+                  })
+                );
+              }}
+            />
+          </div>
+        );
+      })}
+      <label>支払う人</label>
+      <select
+        onChange={(event) => {
+          setPayPerson(Number(event.target.value));
+        }}
+      >
+        <option />
+        {allUsers.map((user, i) => {
+          return <option value={user.id}>{user.name}</option>;
         })}
+      </select>
+      <label>金額</label>
+      <input
+        type="number"
+        onChange={(event) => setUserPay(Number(event.target.value))}
+      />
+
+      <button
+        onClick={() => {
+          setAllUsers(
+            allUsers.map((user, j) => {
+              const payMoney = user.topay ? userPay / userToPay : 0;
+              if (payPerson == j) {
+                return {
+                  ...user,
+                  pay: user.pay + userPay,
+                  buy: user.buy + payMoney,
+                };
+              } else {
+                return {
+                  ...user,
+                  buy: user.buy + payMoney,
+                };
+              }
+            })
+          );
+        }}
+      >
+        Pay
+      </button>
+      <table className="styles.table">
+        <thead>
+          <tr className="styles.tr">
+            <th className="styles.th">Name</th>
+            <th className="styles.th">Pay</th>
+            <th className="styles.th">Buy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {allUsers.map((user, i) => {
+            return (
+              <tr className="styles.tr">
+                <td className="styles.td">{user.name}</td>
+                <td className="styles.td">{String(user.pay)}</td>
+                <td className="styles.td">{String(user.buy)}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
+      <div>{userToPay}</div>
+      <label>初期化する</label>
+      <button
+        onClick={() => {
+          localStorage.clear();
+          setAllUsers([]);
+          setUserToPay(0);
+        }}
+      >
+        Clear
+      </button>
     </main>
   );
 }
